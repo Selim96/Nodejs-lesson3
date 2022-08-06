523f70c1e35fd5b9e84ba480d204ce8bb5445baa
Selim Fazylov <selim.fazylov1@gmail.com>
2022-08-06 16:05:32 +0300

    small fix

--- lesson-8/auth-example/controllers/books/getAll.js
+++ lesson-8/auth-example/controllers/books/getAll.js
@@ -4,8 +4,7 @@ const getAll = async(req, res)=> {
     const {_id: owner} = req.user;
     const {page = 1, limit = 10} = req.query;
     const skip = (page - 1) * limit;
-    const result = await Book.find({owner}, "-createdAt -updatedAt", {skip, limit: Number(limit)})
-                                .populate("owner", "email name");
+    const result = await Book.find({owner}, "-createdAt -updatedAt", {skip, limit: Number(limit)}).populate("owner", "email name");
     res.json(result);
 }
 
